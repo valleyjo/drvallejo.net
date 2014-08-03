bf932da005ab79bd85a7b7692e8c542b49f3c71f
Alex Vallejo <vallejo.alex@gmail.com>
2014-08-03 16:35:27 -0400

    Run rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrvallejoNet::Application.config.secret_key_base = 'c3ff587ffed416ecead2c3c431a49404188f5dfa67767be40cfe9f2ac51c941b479f9653daa9debc6e65a96bcd3a9875a12e0bad14e0f16c93e09f52c3cf3ba0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrvallejoNet::Application.config.session_store :cookie_store, key: '_drvallejo_net_session'
