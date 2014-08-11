d53a31893b9c606c8337d53e38148c4ee0fd4e1e
Alex Vallejo <vallejo.alex@gmail.com>
2014-08-11 18:16:46 -0400

    Update routes.rb. Add active tab selection.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 DrvallejoNet::Application.routes.draw do
+
+  root 'welcome#index'
+  resources :documents
+  resources :videos
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +44,7 @@ DrvallejoNet::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
