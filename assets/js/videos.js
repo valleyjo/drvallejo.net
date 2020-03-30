90aa5ad3a08d8c64e5610447a62c4dd0995ac1f5
Alex Vallejo <vallejo.alex@gmail.com>
2020-03-30 08:14:53 -0700

    Finish videos

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,12 +1,8 @@
-(function() {
-  $(function() {
-    return $("a[data-source-url]").click(function(e) {
-      var iframe_source;
-      e.preventDefault();
-      iframe_source = "<iframe src=\"" + $(this).data("source-url") + "\" width=\"640\" height=\"480\"></iframe>";
-      $("#video-playback").addClass("text-center");
-      return $("#video-playback").html(iframe_source);
-    });
-  });
-
-}).call(this);
+function configure_video(id) {
+  var iframe_source = "<iframe src=\"https://drive.google.com/file/d/";
+  iframe_source += id;
+  iframe_source += "/preview\" width=\"640\" height=\"480\"></iframe>";
+  var playback_area = document.getElementById("video-playback");
+  playback_area.classList.add("text-center");
+  playback_area.innerHTML = iframe_source;
+}
