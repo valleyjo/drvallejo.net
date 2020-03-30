85dae497e581f6e2368f22b58dd838b7f22f7572
Alex Vallejo <vallejo.alex@gmail.com>
2020-03-30 08:39:02 -0700

    Add presentations

--- assets/js/presentations.js
+++ assets/js/presentations.js
@@ -1,12 +1,8 @@
-(function() {
-  $(function() {
-    return $("a[data-source-url]").click(function(e) {
-      var iframe_source;
-      e.preventDefault();
-      iframe_source = "<iframe src=\"" + $(this).data("source-url") + "\" frameborder=\"0\" width=\"400\" height=\"330\" allowfullscreen=\"true\" mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\"></iframe>";
-      $("#presentation-view").addClass("text-center");
-      return $("#presentation-view").html(iframe_source);
-    });
-  });
-
-}).call(this);
+function configure_presentation(id) {
+  var iframe_source = "<iframe src=\"https://docs.google.com/presentation/d/"
+  iframe_source += id;
+  iframe_source += "/embed?start=true&amp;loop=false&amp;delayms=30000\" frameborder=\"0\" width=\"400\" height=\"330\" allowfullscreen=\"true\" mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\"></iframe>";
+  var playback_area = document.getElementById("presentation-view")
+  playback_area.classList.add("text-center");
+  playback_area.innerHTML = iframe_source;
+}
